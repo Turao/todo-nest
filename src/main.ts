df158163b04da5a2b3e418f69df5bcc1e2a3510c
turao <lenz.arthur@gmail.com>
2019-06-03 08:04:25 -0300

    Add helmet, CORS and CSURF

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(helmet());
+  app.use(csurf());
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('To-Do Nest Project')
